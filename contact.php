2dc1c391e06d3aaf4d8887e60e2c4510e72815dc
John <John.Arscott@hotmail.com>
2022-08-02 20:42:00 +0000

    Save three

--- contact.php
+++ contact.php
@@ -33,10 +33,10 @@
                                 <a href="resume.html" class="hvr-sweep-to-bottom"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span>Resume</span></a>
                             </li>
                             <li class="col-6 col-sm-3 list-inline-item ui-menu-color-contact menuitem">
-                                <a href="contact.html" class="hvr-sweep-to-bottom"><i class="fa fa-comment-o" aria-hidden="true"></i><span>Contact</span></a>
+                                <a href="contact.php" class="hvr-sweep-to-bottom"><i class="fa fa-comment-o" aria-hidden="true"></i><span>Contact</span></a>
                             </li>
                             <li class="col-6 col-sm-3 list-inline-item ui-menu-color-download menuitem">
-                                <a href="assets/cv/sample-cv.pdf" target="_blank" class="hvr-sweep-to-bottom"><i class="fa fa-download" aria-hidden="true"></i><span>Download CV</span></a>
+                                <a href="assets/cv/jca-dev.pdf" target="_blank" class="hvr-sweep-to-bottom"><i class="fa fa-download" aria-hidden="true"></i><span>Download CV</span></a>
                             </li>
                         </ul>
                     </div>
@@ -47,17 +47,18 @@
     <section class="container-fluid">
         <div class="row bg-color-contact">
             <div class="col">
-                <h3 class="contact-heading uppercase text-center">Got a project in mind?</h3>
-                <h5 class="uppercase text-center">Let's work together!</h5>
+                <h3 class="contact-heading uppercase text-center">Interested in employing me?</h3>
+                <h5 class="uppercase text-center">Lets create something amazing!</h5>
 
                 <div class="center-form">
-                    <form>
+                    <form action=”mailto:john.arscott@hotmail.com method=”POST” enctype=”multipart/form-data” name=”EmailForm”>
                         <input type="text" name="name" id="fullname" class="form-control" placeholder="Name" required />
                         <input type="email" name="email" id="emailaddress" class="form-control" placeholder="Email" required />
-                        <textarea rows="5" name="projectsummary" id="projectsummary" class="form-control" placeholder="Project description" required></textarea>
+                        <textarea rows="5" name="projectsummary" id="projectsummary" class="form-control" placeholder="Enter vacancey information e.g job title, job description, salary, benifits and a little message." required></textarea>
                         <div class="form-row text-center">
                             <div class="col">
-                                <button type="submit" class="btn btn-secondary">Send Project Request</button>
+                                <button type="submit" class="btn btn-secondary">Submit your interest</button>
+                                <a href="contact.html"><button type="submit" class="btn btn-secondary">Email</button></a>
                             </div>
                         </div>
                     </form>
@@ -72,14 +73,15 @@
             <div class="col-sm-4">
                 <h5 class="uppercase general-sub">About</h5>
                 <p class="inline-block">
-                    Trainee Full Stack Web Developer. Skilled in everything from HTML to Heroku. The capacity for structural and design thinking.
+                    Trainee Full Stack Web Developer. Skilled in everything from HTML to Heroku. The capacity for
+                    structural and design thinking. Lover of Agile methodology.
                 </p>
             </div>
             <div class="col-sm-4">
                 <h5 class="uppercase general-sub">Download</h5>
                 <p class="inline-block">
                     Need a printable version of my CV? Download it here.
-                    <a href="#" target="_blank" class="cv-pdf">
+                    <a href="assets/cv/jca-dev.pdf" target="_blank" class="cv-pdf">
                         <i class="fa fa-download" aria-hidden="true"></i>
                         <span class="sr-only">Download link.</span>
                     </a>
@@ -89,13 +91,13 @@
                 <h5 class="uppercase general-sub">Social</h5>
                 <ul class="list-inline social-links">
                     <li class="list-inline-item">
-                        <a target="_blank" href="#">
+                        <a target="_blank" href="https://www.facebook.com/MrLazyCloud">
                             <i class="fa fa-facebook" aria-hidden="true"></i>
                             <span class="sr-only">Facebook</span>
                         </a>
                     </li>
                     <li class="list-inline-item">
-                        <a target="_blank" href="#">
+                        <a target="_blank" href="https://www.linkedin.com/in/jca-dev/">
                             <i class="fa fa-linkedin" aria-hidden="true"></i>
                             <span class="sr-only">LinkedIn</span>
                         </a>
