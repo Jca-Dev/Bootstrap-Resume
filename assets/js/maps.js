081991a83b812aa5f4cc216a8d7e582555f71415
John <John.Arscott@hotmail.com>
2022-09-06 18:53:47 +0000

    Update full site (Incomplete)

---
+++ assets/js/maps.js
@@ -0,0 +1,25 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 50.5294916, lng: -3.6024662},
+        { lat: 45.4656888, lng: -73.7476701},
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
